53d3d2fe19a85493b9e942ba16e10f242bdb38dd
DctorWei1314 <61504793+DctorWei1314@users.noreply.github.com>
2021-12-07 00:42:09 +0800

    data sharing test finish

---
+++ data sharing/call_once.cpp
@@ -0,0 +1,79 @@
+std::shared_ptr<some_resource> resource_ptr;
+void foo()
+{
+  if(!resource_ptr)
+  {
+    resource_ptr.reset(new some_resource);  // 1
+  }
+  resource_ptr->do_something();
+}
+std::shared_ptr<some_resource> resource_ptr;
+std::mutex resource_mutex;
+
+void foo()
+{
+  std::unique_lock<std::mutex> lk(resource_mutex);  // 所有线程在此序列化 
+  if(!resource_ptr)
+  {
+    resource_ptr.reset(new some_resource);  // 只有初始化过程需要保护 
+  }
+  lk.unlock();
+  resource_ptr->do_something();
+}
+
+//data race
+void undefined_behaviour_with_double_checked_locking()
+{
+  if(!resource_ptr)  // 1
+  {
+    std::lock_guard<std::mutex> lk(resource_mutex);
+    if(!resource_ptr)  // 2
+    {
+      resource_ptr.reset(new some_resource);  // 3
+    }
+  }
+  resource_ptr->do_something();  // 4
+}
+
+//call_one
+std::shared_ptr<some_resource> resource_ptr;
+std::once_flag resource_flag;  // 1
+
+void init_resource()
+{
+  resource_ptr.reset(new some_resource);
+}
+
+void foo()
+{
+  std::call_once(resource_flag,init_resource);  // 可以完整的进行一次初始化
+  resource_ptr->do_something();
+}
+
+//class example
+class X
+{
+private:
+  connection_info connection_details;
+  connection_handle connection;
+  std::once_flag connection_init_flag;
+
+  void open_connection()
+  {
+    connection=connection_manager.open(connection_details);
+  }
+public:
+  X(connection_info const& connection_details_):
+      connection_details(connection_details_)
+  {}
+  void send_data(data_packet const& data)  // 1
+  {
+    std::call_once(connection_init_flag,&X::open_connection,this);  // 2
+    connection.send_data(data);
+  }
+  data_packet receive_data()  // 3
+  {
+    std::call_once(connection_init_flag,&X::open_connection,this);  // 2
+    return connection.receive_data();
+  }
+};
---
+++ data sharing/mutex.cpp
@@ -0,0 +1,59 @@
+#include <thread>
+#include <iostream>
+#include <assert.h>
+#include <mutex>
+#include <list>
+#include <algorithm>
+std::list<int> some_list;    // 1
+std::mutex some_mutex;    // 2
+
+void add_to_list(int new_value)
+{
+  std::lock_guard<std::mutex> guard(some_mutex);    // 3
+  some_list.push_back(new_value);
+}
+
+bool list_contains(int value_to_find)
+{
+  std::lock_guard<std::mutex> guard(some_mutex);    // 4
+  return std::find(some_list.begin(),some_list.end(),value_to_find) != some_list.end();
+}
+
+class some_data
+{
+  int a;
+  std::string b;
+public:
+  void do_something();
+};
+
+class data_wrapper
+{
+private:
+  some_data data;
+  std::mutex m;
+public:
+  template<typename Function>
+  void process_data(Function func)
+  {
+    std::lock_guard<std::mutex> l(m);
+    func(data);    // 1 传递“保护”数据给用户函数
+  }
+};
+
+some_data* unprotected;
+
+void malicious_function(some_data& protected_data)
+{
+  unprotected=&protected_data;
+}
+
+data_wrapper x;
+void foo()
+{
+  x.process_data(malicious_function);    // 2 传递一个恶意函数
+  unprotected->do_something();    // 3 在无保护的情况下访问保护数据
+}
+int main(){
+    
+}
