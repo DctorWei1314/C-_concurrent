5bb97d4949b064a929a5b623cb2534a2c09483b4
DctorWei1314 <61504793+DctorWei1314@users.noreply.github.com>
2021-12-11 14:54:29 +0800

    Update memory_order_acq_rel.cpp

--- memory_model/memory_order_acq_rel.cpp
+++ memory_model/memory_order_acq_rel.cpp
@@ -39,3 +39,28 @@ int main()
   d.join();
   assert(z.load()!=0); // 3
 }
+// std::atomic<bool> x,y;
+// std::atomic<int> z;
+
+// void write_x_then_y()
+// {
+//   x.store(true,std::memory_order_relaxed);  // 1 
+//   y.store(true,std::memory_order_release);  // 2
+// }
+// void read_y_then_x()
+// {
+//   while(!y.load(std::memory_order_acquire));  // 3 自旋，等待y被设置为true
+//   if(x.load(std::memory_order_relaxed))  // 4
+//     ++z;
+// }
+// int main()
+// {
+//   x=false;
+//   y=false;
+//   z=0;
+//   std::thread a(write_x_then_y);
+//   std::thread b(read_y_then_x);
+//   a.join();
+//   b.join();
+//   assert(z.load()!=0);  // 5
+// }
